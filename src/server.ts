a071b2430c6aa36a4172a2bb2646bb9a17f08e88
djid-tif <madjid.tifour.mt@gmail.com>
2025-02-07 10:02:56 +0100

    fix cors error

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import http from 'http';
-import path from 'path';
 import apiRoutes from './routes/api';
+import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
 app.use(express.json());
 app.use('/api', apiRoutes);
 
